861f6ab2aa180e543149911efadca99fce9b3f28
jun kawakami <http.kj.com@gmail.com>
2025-06-21 16:33:45 +0900

    update efi graphics protocol

--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 # Rust OS memo
 
+[Rust UEFI Book](https://rust-osdev.github.io/uefi-rs/introduction.html)
+[UEFI 仕様 2.11](https://uefi.org/specs/UEFI/2.11/)
+
 ## QEMUを使ったバイナリファイルの実行
 
 まず作成したバイナリファイルがブート可能かを調べる
@@ -20,7 +23,152 @@ $ qemu-system-x86_64 -drive file=img.hex,format=raw -nographic
 
 ## Rustについて
 
-#### Rustのツールチェイン
+### ライフタイム
+
+全参照にはライフタイムがあり、参照を使用する関数や構造体にはライフタイム引数を指定する必要があります。
+
+```rust
+stract Example<'a> {
+    raw: &'a [u8; 256]
+}
+
+fn lifetime_func<'a>(input: &'a str) -> &'a str {
+    input
+}
+```
+
+#### ライフタイム省略
+
+[参考](https://doc.rust-jp.rs/book-ja/ch10-03-lifetime-syntax.html#%E3%83%A9%E3%82%A4%E3%83%95%E3%82%BF%E3%82%A4%E3%83%A0%E7%9C%81%E7%95%A5)
+
+コンパイラの参照解析に落とし込まれたパターンは、ライフタイム省略規則と呼ばれます。
+省略規則は、完全な推論を提供しません。
+
+関数やメソッドの引数のライフタイムは、入力ライフタイムと呼ばれ、 戻り値のライフタイムは出力ライフタイムと称されます。
+
+> 毎回書くと冗長になるため、よくあるパターンだけは自動的に推論するように
+
+#### 規則1
+---
+「参照である各引数は、独自のライフタイム引数を得る」
+
+つまり：
+
+```rust
+fn foo<'a>(x: &'a i32);         // 引数1つ → ライフタイム 'a
+fn foo<'a, 'b>(x: &'a i32, y: &'b i32);  // 引数2つ → 'a, 'b
+```
+省略して書いても、コンパイラはそれぞれの参照に個別のライフタイムをつけて解釈します。
+
+
+
+#### 規則2
+---
+「1つだけ入力ライフタイムがあるなら、そのライフタイムがすべての出力に代入される」
+
+```rust
+
+fn get<'a>(x: &'a i32) -> &'a i32  // 明示
+// 省略形:
+fn get(x: &i32) -> &i32            // 自動的に上記と等価
+
+```
+
+
+#### 規則3
+---
+
+「メソッドで、入力に &self または &mut self があるなら、self のライフタイムが出力に使われる」
+
+```rust
+impl<'a> Wrapper<'a> {
+    fn get(&self) -> &str          // ライフタイム省略されているが…
+    // 実際は:
+    fn get<'a>(&'a self) -> &'a str
+}
+```
+
+
+### スタティックライフタイム
+
+[参考](https://doc.rust-jp.rs/rust-by-example-ja/scope/lifetime/static_lifetime.html)
+
+Rustにおける`'static`ライフタイムは、最も長いライフタイムです。
+
+参照のライフタイムが`'static`であることは、参照が指し示す値がプログラムの実行中に渡って生き続けることを示します。 また、より短いライフタイムに圧縮することも可能です。
+
+#### 文字列リテラル
+---
+`"これは静的文字列"`はバイナリに埋め込まれるため、プログラムの寿命中ずっと有効です。
+
+
+```rust
+let msg: &'static str = "これは静的文字列";
+```
+
+#### static変数
+---
+CONFIGは 'static ライフタイムを持ち、プログラム終了まで保持されます。
+
+```rust
+static CONFIG: Config = Config::new();
+```
+
+#### ヒープに確保して、明示的にリークさせた値
+---
+Box::leakにより、ヒープ上に確保された値の所有権が失われ、プログラム終了まで残ります。
+
+
+```rust
+let s: &'static str = Box::leak(Box::new("leaked string".to_string()));
+```
+
+
+#### `'static`と`&'static`の違い
+
+- `&'static T`
+  - 値`T`への参照が`'static`(参照先がプログラム中ずっと有効)
+- `'static T`
+  - 所有する値自体が`'satatic`(例：static変数、グローバル変数等)
+
+### null未チェックバージョン
+- [参照1](https://doc.rust-lang.org/std/primitive.pointer.html#null-unchecked-version)
+
+- [参照2](https://doc.rust-lang.org/reference/lifetime-elision.html)
+
+Rustでは、生ポインタ（*const T や *mut T）を安全な参照（&T や &mut T）に変換する操作は、unsafe を伴います。これはコンパイラが「そのポインタが有効で非nullである」と保証できないためです。
+
+このような変換は以下のように行います：
+
+```rust
+unsafe { &*value }
+```
+これは、ポインタが指すメモリ位置から参照を作るという意味で、「null未チェックバージョン」と呼ばれます。
+ポインタがnullである場合、未定義動作（undefined behavior）になります。
+
+
+##### ❗補足：関数内で生成したデータの参照を返すケース
+以下のような「関数内で生成した一時的な値の参照を返す」ことは安全でもunsafeでも不可能です：
+
+```rust
+fn get_ref() -> &i32 {
+    let x = 42;
+    &x  // ❌ これはコンパイルエラー。xは関数のスコープで消えるため
+}
+```
+
+このようなケースはライフタイムが明らかに無効になるため、Rustの借用チェッカーが静的に拒否します。
+
+ただし、グローバル変数やBox::leak()、static変数など、寿命が十分に長いデータからであれば参照は返せます。以下のように：
+
+```rust
+fn get_ref() -> &'static i32 {
+    Box::leak(Box::new(42))  // ✅ 'static な参照を返す
+}
+```
+
+
+## Rustのツールチェイン
 
 バージョンを固定するには`rust-toolchain.yaml`というファイルをRustのプロジェクト内に作成しそれぞれ項目を設定する
 
--- work/repo/wasabi/src/main.rs
+++ work/repo/wasabi/src/main.rs
@@ -1,17 +1,141 @@
 #![no_std]
 #![no_main]
+#![feature(offset_of)]
 
-#[no_mangle]
-fn efi_main() {
-    // println!("Hello, world!");
-    loop {
-        // println!("Looping...");
-    }
+use core::mem::offset_of;
+use core::mem::size_of;
+use core::panic::PanicInfo;
+use core::ptr::null_mut;
+use core::slice;
+
+
+type EfiVoid = u8;
+type EfiHandle = u64;
+type Result<T> = core::result::Result<T, &'static str>;
+
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+#[must_use]
+#[repr(u64)]
+enum EfiStatus {
+    Success = 0,
 }
 
-use core::panic::PanicInfo;
+#[repr(C)]
+struct EfiBootServicesTable {
+    _reserved0: [u64; 40],
+    locate_protocol: extern "win64" fn(
+        protocol: *const EfiGuid,
+        registration: *const EfiVoid,
+        interface: *mut *mut EfiVoid,
+    ) -> EfiStatus,
+}
+const _: () = assert!(offset_of!(EfiBootServicesTable, locate_protocol) == 320);
+
+
+/// https://uefi.org/specs/UEFI/2.11/04_EFI_System_Table.html
+#[repr(C)]
+struct EfiSystemTable {
+    _reserved0: [u64; 12],
+    pub boot_services: &'static EfiBootServicesTable,
+}
+const _: () = assert!(offset_of!(EfiSystemTable, boot_services) == 96);
+
+///EFI Graphics Output Protocolを示すGUIDの定義
+#[repr(C)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct EfiGuid {
+    pub data0: u32,
+    pub data1: u16,
+    pub data2: u16,
+    pub data3: [u8; 8],
+}
+
+// EFI_GRAPHICS_OUTPUT_MODE_INFORMATIONの構造体
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocolPixelInfo {
+    version: u32,
+    pub horizontalresolution: u32, // 水平方向の画素数
+    pub verticalresolution: u32,   // 垂直方向の画素数　
+    _padding0: [u32; 5],
+    pub pixels_per_scan_line: u32, // 水平方向のデータに含まれる画素数
+}
+const _: () = assert!(size_of::<EfiGraphicsOutputProtocolPixelInfo>() == 36);
+
+
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocolMode<'a> {
+    pub max_mode: u32,
+    pub mode: u32,
+    pub info: &'a EfiGraphicsOutputProtocolPixelInfo,
+    pub size_of_info: u64,
+    pub frame_buffer_base: usize,
+    pub frame_buffer_size: usize,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+struct EfiGraphicsOutputProtocol<'a> {
+    reserved: [u64; 3],
+    pub mode: &'a EfiGraphicsOutputProtocolMode<'a>,
+}
+
+
+// https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-graphics-output-protocol
+const EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID : EfiGuid = EfiGuid {
+    data0: 0x9042a9de,
+    data1: 0x23dc,
+    data2: 0x4a38,
+    data3: [0x96,0xfb,0x7a,0xde,0xd0,0x80,0x51,0x6a],
+};
+
+
+/// EfiGraphicsOutputProtocolをUEFIから取得する関数
+///
+/// # Arguments
+///
+/// - `efi_system_table` (&EfiSystemTable): EFI System Table
+///
+/// # Returns
+/// `Result<&'a EfiGraphicsOutputProtocol<'a>>`
+fn locate_graphic_protocol<'a>(efi_system_table: &EfiSystemTable) -> Result<&'a EfiGraphicsOutputProtocol<'a>> {
+    let mut graphic_output_protocol = null_mut::<EfiGraphicsOutputProtocol>();
+    let status = (efi_system_table.boot_services.locate_protocol)(
+        &EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID,
+        null_mut::<EfiVoid>(),
+        &mut graphic_output_protocol 
+            as *mut *mut EfiGraphicsOutputProtocol 
+            as *mut *mut EfiVoid,
+    );
+
+    if status != EfiStatus::Success {
+        return Err("Failed to locate graphics output protocol");
+    }
+    Ok(unsafe { &*graphic_output_protocol })
+}
+
+
+#[no_mangle]
+fn efi_main(_img_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
+    let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
+    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
+    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
+    let vram = unsafe {
+        slice::from_raw_parts_mut(
+            vram_addr as *mut u32,
+            vram_byte_size / size_of::<u32>()
+        )
+    };
+    for e in vram {
+        *e = 0xfff0ff;
+    }
+    loop {}
+}
+
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
